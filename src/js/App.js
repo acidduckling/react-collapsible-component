03fd67217268a6e5f0f8f66d96bceb8c9ca7a7f4
Clayton Wakefield <acidduckling@gmail.com>
2018-07-15 19:40:09 +1000

    Created Collapsible component

--- src/js/App.js
+++ src/js/App.js
@@ -1,50 +1,30 @@
 import React from 'react';
 import image from '../images/expand-vertical-4.svg';
+import Collapsible from './Collapsible';
 
 class App extends React.Component {
-    render() {
-        return (
-            <div>
-                <header>
-                    <img src={image} />
-                    <h1>Collapsible Content</h1>
-                </header>
-                <div className="content">
-                    <div className="panel-group">
-                        <div className="panel">
-                            <div className="panel-heading">
-                                <h2>Overview</h2>
-                            </div>
-                            <div className="panel-collapse">
-                                <div className="panel-body">
-                                    Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute.
-                                </div>
-                            </div>
-                        </div>
-                        <div className="panel">
-                            <div className="panel-heading">
-                                <h2>Features</h2>
-                            </div>
-                            <div className="panel-collapse">
-                                <div className="panel-body">
-                                    Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute.
-                                </div>
-                            </div>
-                        </div>
-                        <div className="panel">
-                            <div className="panel-heading">
-                                <h2>Reviews</h2>
-                            </div>
-                            <div className="panel-collapse">
-                                <div className="panel-body">
-                                    Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute.
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <header>
+          <img src={image} />
+          <h1>Collapsible Content</h1>
+        </header>
+        <div className="content">
+          <div className="panel-group">
+            <Collapsible title="Overview">
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Id quis modi harum similique. Laudantium incidunt accusamus asperiores, aperiam sequi sed!</p>
+            </Collapsible>
+            <Collapsible title="Features">
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Id quis modi harum similique. Laudantium incidunt accusamus asperiores, aperiam sequi sed!</p>
+            </Collapsible>
+            <Collapsible title="Software">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio, neque?</p>
+            </Collapsible>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 export default App;
