4a4ae884c912fbf0908e1b7e628085d6f963f391
Clayton Wakefield <acidduckling@gmail.com>
2018-07-16 09:33:52 +1000

    Complete collapsible Panel

--- src/js/Collapsible.js
+++ src/js/Collapsible.js
@@ -7,18 +7,23 @@ class Collapsible extends React.Component {
     this.state = {
       isExpanded: false
     };
+    this.panelHeight = React.createRef();
   }
 
   handleToggle(e) {
     e.preventDefault();
+    //console.log(this.panelHeight.current.clientHeight);
+
     this.setState({
-      isExpanded: !this.state.isExpanded
+      isExpanded: !this.state.isExpanded,
+      height: this.panelHeight.current.clientHeight
     });
   }
 
   render() {
     const { title, children } = this.props;
-    const { isExpanded } = this.state;
+    const { isExpanded, height } = this.state;
+    const currentHeight = isExpanded ? height : 0;
     return (
       <div
         className={`panel ${isExpanded ? 'is-expanded' : ''}`}
@@ -27,15 +32,17 @@ class Collapsible extends React.Component {
         <div className="panel-heading">
           <h2>{title}</h2>
         </div>
-        <div className="panel-collapse">
-          <div className="panel-body">{children}</div>
+        <div className="panel-collapse" style={{ height: currentHeight + 'px'}}>
+          <div className="panel-body" ref={this.panelHeight}>
+            {children}
+          </div>
         </div>
       </div>
     );
   }
 }
 
-Collapsible.PropTypes = {
+Collapsible.propTypes = {
   title: PropTypes.string
 };
 
